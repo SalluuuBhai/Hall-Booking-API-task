8075acb529e2aa406533bd22adf7462bcaf52d18
SalluuuBhai <md.salmanfaris03@gmail.com>
2024-01-11 21:22:41 +0530

    first commit

---
+++ db.js
@@ -0,0 +1,6 @@
+// const dbName = 'HallBooking';
+DB_NAME = 'HallBooking'
+
+const dbURL = `mongodb+srv://mdsalmanfaris03:salman3510@cluster0.pet40j4.mongodb.net/${DB_NAME}`;
+
+module.exports = {dbURL};
